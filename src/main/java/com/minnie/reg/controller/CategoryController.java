c9a58056f6f057372f85d04e3249afbdce0bdb58
hongjinyuminnie@gmail.com <hongjinyuminnie@gmail.com>
2022-12-14 19:40:01 -0500

    新添菜品

---
+++ src/main/java/com/minnie/reg/controller/CategoryController.java
@@ -0,0 +1,56 @@
+package com.minnie.reg.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.minnie.reg.common.R;
+import com.minnie.reg.entity.Category;
+import com.minnie.reg.entity.Employee;
+import com.minnie.reg.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+//分类管理
+@Slf4j
+@RestController
+@RequestMapping("/category")
+
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+    //新增分类
+
+    @PostMapping
+    public R<String> save(@RequestBody Category category){
+        log.info("新增菜品分类：{}",category.toString());
+
+        categoryService.save(category);
+
+        return R.success("Successfully added food menu");
+    }
+
+    /*
+    员工信息分页查询
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize){
+        log.info("page:{},page_size:{}",page,pageSize);
+        //构造分页构造器
+        Page<Category> pageinfo = new Page<>(page,pageSize);
+
+        //构造条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper();
+
+        //添加排序条件
+        queryWrapper.orderByAsc(Category::getSort);
+
+        //执行查询
+        categoryService.page(pageinfo,queryWrapper);
+
+        return R.success(pageinfo);
+
+    }
+}
---
+++ src/main/java/com/minnie/reg/service/CategoryService.java
@@ -0,0 +1,7 @@
+package com.minnie.reg.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.minnie.reg.entity.Category;
+
+public interface CategoryService extends IService<Category> {
+}
